f224bdafa2ef006655b3a83a1287d36c5eb99f1a
1t1e1 <1t1ebir@gmail.com>
2020-07-07 13:57:59 +0300

    axios request atildi.

--- src/Component/HCharts.js
+++ src/Component/HCharts.js
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import Highcharts from "highcharts";
 import HighchartsReact from "highcharts-react-official";
+
+const apikey = "97cee60fe2193b383cd8377301901a80";
 
 const options = {
 	chart: {
@@ -17,6 +20,24 @@ const options = {
 };
 
 export default function HCharts() {
+	const [state, setstate] = useState();
+
+	useEffect(() => {
+		axios
+			.get(
+				`http://ws.audioscrobbler.com/2.0/?method=geo.gettoptracks&country=spain&api_key=${apikey}&format=json&limit=10`
+			)
+			.then(function (response) {
+				console.log(response.data);
+			})
+			.catch(function (error) {
+				console.log(error);
+			});
+
+		return () => {
+			// will
+		};
+	}, []);
 	return (
 		<div>
 			<p> Hello</p>
