074d77c71617189d087177f7220b2aab8bd90a55
1t1e1 <1t1ebir@gmail.com>
2020-07-08 00:04:58 +0300

    Updates

---
+++ precache-manifest.fe95274f8c19d9ccaee3152e7304d547.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4288c0e629f9bd782773ae170cc83429",
+    "url": "/last-fm-Challenge/index.html"
+  },
+  {
+    "revision": "7499ec463ce6d771046a",
+    "url": "/last-fm-Challenge/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b5ec4538056c120a5805",
+    "url": "/last-fm-Challenge/static/js/2.d3e339e8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/last-fm-Challenge/static/js/2.d3e339e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7499ec463ce6d771046a",
+    "url": "/last-fm-Challenge/static/js/main.f5340847.chunk.js"
+  },
+  {
+    "revision": "6731236400e36842c077",
+    "url": "/last-fm-Challenge/static/js/runtime-main.32c07ed6.js"
+  }
+]);
